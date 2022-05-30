5ea240744402faa8e35ba8e21f545d03fe29b34c
matheuslimajv <matnsfw017@gmail.com>
2022-05-29 22:04:47 -0300

    changes in menu

--- src/Game.java
+++ src/Game.java
@@ -54,25 +54,33 @@ public class Game{
                     menu();
                 }
             }
-
-        } catch (Exception e) {
-            System.out.print("An error occurs");
-        }
+	} catch (Exception e) {                                     System.out.print("An error occurs");                }
     }
-        public static void tutorial(){
-            System.out.println("Tutorial");
-        }
 
+        public static void tutorial(){
+	clearScreen();	
+	try{
+            System.out.println("""
+			    \t| 1 | 2 | 3 |                                         \t| 4 | 5 | 6 |                                         \t| 7 | 8 | 9 |
+			    """);
+	    System.out.println();
+	    String text = "The game is played typing a corresponding number in the board";
+	    for(int i = 0; i < text.length(); i++){
+		    System.out.print(text.charAt(i));
+		    Thread.sleep(150);
+        }
+	    System.out.println();
+	    System.out.println("Look a example below");
+	    System.out.println();
+	    
+	} catch (Exception e) {                                     System.out.print("An error occurs");                }
+	}
         public static void clearScreen(){
             System.out.print("\033[H\033[2J");
             System.out.flush();
         }
-
         public static void main(String[] args){
         clearScreen();
         menu();
-        }
-
-
-
+    	    }
     }
